894d6c05235f300c2384e168e58b910cfea03c79
David Liu <shaowei.liu@gmail.com>
2017-11-24 17:38:27 +0800

    added fake chart view for panels and devices in dashboard

---
+++ src/views/dashboard/PanelsChart.vue
@@ -0,0 +1,78 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+// const brandPrimary = '#20a8d8'
+// const brandSuccess = '#4dbd74'
+const brandInfo = '#63c2de'
+// const brandDanger = '#f86c6b'
+
+function convertHex (hex, opacity) {
+  hex = hex.replace('#', '')
+  const r = parseInt(hex.substring(0, 2), 16)
+  const g = parseInt(hex.substring(2, 4), 16)
+  const b = parseInt(hex.substring(4, 6), 16)
+
+  const result = 'rgba(' + r + ',' + g + ',' + b + ',' + opacity / 100 + ')'
+  return result
+}
+
+function random (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+export default Line.extend({
+  props: ['height'],
+  mounted () {
+    var elements = 7
+    var data1 = []
+
+    for (var i = 0; i <= elements; i++) {
+      data1.push(random(50, 200))
+    }
+    this.renderChart({
+      labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+      datasets: [
+        {
+          label: 'Panels',
+          backgroundColor: convertHex(brandInfo, 10),
+          borderColor: brandInfo,
+          pointHoverBackgroundColor: '#fff',
+          borderWidth: 2,
+          data: data1
+        }
+      ]
+    }, {
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          gridLines: {
+            drawOnChartArea: false
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            maxTicksLimit: 5,
+            stepSize: Math.ceil(250 / 5),
+            max: 250
+          },
+          gridLines: {
+            display: true
+          }
+        }]
+      },
+      elements: {
+        point: {
+          radius: 0,
+          hitRadius: 10,
+          hoverRadius: 4,
+          hoverBorderWidth: 3
+        }
+      }
+    })
+  }
+})
+</script>
